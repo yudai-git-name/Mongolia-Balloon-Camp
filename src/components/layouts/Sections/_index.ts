7b82494f134f9b4cda07b94f3bf970f7d9fd5747
鈴木雄大 <user@YSMacBook.local>
2025-02-27 19:56:11 +0900

    次回tableから

---
+++ src/components/layouts/Sections/_index.ts
@@ -0,0 +1,8 @@
+export { default as Charm } from './Charm.astro';
+export { default as Balloon } from './Balloon.astro';
+export { default as Nomad } from './Nomad.astro';
+export { default as Camp } from './Camp.astro';
+export { default as Voice } from './Voice.astro';
+export { default as Qa } from './Qa.astro';
+export { default as Message } from './Message.astro';
+export { default as Contact } from './Contact.astro';
